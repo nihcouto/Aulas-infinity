18643384c01739c96af51d07dc9c562a2ca40af3
nihcouto <nicolecoutom@icloud.com>
2025-05-03 10:12:47 -0300

    Add files via upload
    aula do dia 03/05, antes de iniciar o programa real de biblioteca

--- sistema_biblioteca/library_system/accounts/models.py
+++ sistema_biblioteca/library_system/accounts/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class Usuario(models.Model):
+    nome = models.CharField(max_length=255)
+    email = models.EmailField(unique=True)
+
+    def __str__(self):
+        return f"{self.nome} - {self.email}"
